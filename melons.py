ba00ddf9b945b4b4508c35bf569de7147a563f22
Kaytee Cannon <kaytee.cannon@me.com>
2020-10-09 20:29:26 +0000

    Refactors InternationalMelonOrder init method
    Co-authored-by: exsma <essma@outlook.com>

--- melons.py
+++ melons.py
@@ -30,7 +30,6 @@ class DomesticMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
         super().__init__(species, qty, 'domestic', 0.08)
-        self.shipped = False 
         
 
     def mark_shipped(self):
@@ -44,12 +43,9 @@ class InternationalMelonOrder(AbstractMelonOrder):
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
-        self.species = species
-        self.qty = qty
-        self.shipped = False 
+        super().__init__(species, qty, 'international', 0.17)
         self.country_code = country_code
-        self.order_type = "international"
-        self.tax = 0.17
+
 
     def get_total(self):
         """Calculate price, including tax."""
